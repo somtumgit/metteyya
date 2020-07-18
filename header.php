4cf6c227077c6ed41e179756273e0a74c0195ca7
somtumgit <oajkk657@gmail.com>
2020-07-18 09:13:37 +0700

    first commit

--- header.php
+++ header.php
@@ -1,3 +1,26 @@
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v7.0'
+        });
+      };
+
+      (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat"
+      attribution=setup_tool
+      page_id="112639467184555">
+    </div>
 <div class="container">
     <nav class="navbar navbar-expand-md " style="color:#fff;background-color: #B56881;">
         <a href="index.php" class="navbar-brand text-white">Metteyya</a>
